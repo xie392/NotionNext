d21debc7a4fc0f82d592ea9c38465c05ffc9b053
xie392 <ymzdjiang@qq.com>
2024-04-16 10:31:11 +0800

    chore: 去除底部一些不必要的内容，去除gitbook主题下的左侧的小红点

--- themes/gitbook/components/NavPostItem.js
+++ themes/gitbook/components/NavPostItem.js
@@ -1,9 +1,10 @@
+'use client'
 import BlogPostCard from './BlogPostCard'
 import { useState } from 'react'
 import Collapse from '@/components/Collapse'
-import Badge from '@/components/Badge'
-import { siteConfig } from '@/lib/config'
-import CONFIG from '../config'
+// import Badge from '@/components/Badge'
+// import { siteConfig } from '@/lib/config'
+// import CONFIG from '../config'
 
 /**
  * 导航列表
@@ -12,7 +13,7 @@ import CONFIG from '../config'
  * @returns {JSX.Element}
  * @constructor
  */
-const NavPostItem = (props) => {
+const NavPostItem = props => {
   const { group } = props
   const [isOpen, changeIsOpen] = useState(group?.selected)
 
@@ -20,28 +21,44 @@ const NavPostItem = (props) => {
     changeIsOpen(!isOpen)
   }
 
-  const groupHasLatest = group?.items?.some(post => post.isLatest)
+  // const groupHasLatest = group?.items?.some(post => post.isLatest)
 
   if (group?.category) {
-    return <>
-            <div onClick={toggleOpenSubMenu}
-                className='select-none relative flex justify-between text-sm cursor-pointer p-2 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600' key={group?.category}>
-                <span>{group?.category}</span>
-                <div className='inline-flex items-center select-none pointer-events-none '><i className={`px-2 fas fa-chevron-left transition-all opacity-50 duration-200 ${isOpen ? '-rotate-90' : ''}`}></i></div>
-                {groupHasLatest && siteConfig('GITBOOK_LATEST_POST_RED_BADGE', false, CONFIG) && !isOpen && <Badge/>}
+    return (
+      <>
+        <div
+          onClick={toggleOpenSubMenu}
+          className='select-none relative flex justify-between text-sm cursor-pointer p-2 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600'
+          key={group?.category}>
+          <span>{group?.category}</span>
+          <div className='inline-flex items-center select-none pointer-events-none '>
+            <i
+              className={`px-2 fas fa-chevron-left transition-all opacity-50 duration-200 ${isOpen ? '-rotate-90' : ''}`}></i>
+          </div>
+          {/* 去除小红点 */}
+          {/* {groupHasLatest &&
+            siteConfig('GITBOOK_LATEST_POST_RED_BADGE', false, CONFIG) &&
+            !isOpen && <Badge />} */}
+        </div>
+        <Collapse isOpen={isOpen} onHeightChange={props.onHeightChange}>
+          {group?.items?.map(post => (
+            <div key={post.id} className='ml-3 border-l'>
+              <BlogPostCard className='text-sm ml-3' post={post} />
             </div>
-            <Collapse isOpen={isOpen} onHeightChange={props.onHeightChange}>
-                {group?.items?.map(post => (<div key={post.id} className='ml-3 border-l'>
-                    <BlogPostCard className='text-sm ml-3' post={post} /></div>))
-                }
-            </Collapse>
-        </>
+          ))}
+        </Collapse>
+      </>
+    )
   } else {
-    return <>
-            {group?.items?.map(post => (<div key={post.id} >
-                <BlogPostCard className='text-sm py-2' post={post} /></div>))
-            }
-        </>
+    return (
+      <>
+        {group?.items?.map(post => (
+          <div key={post.id}>
+            <BlogPostCard className='text-sm py-2' post={post} />
+          </div>
+        ))}
+      </>
+    )
   }
 }
 
